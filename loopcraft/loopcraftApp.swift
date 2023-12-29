61167a65978f674b96276e865790dd45fb742d87
Siddharth Gurumurthi <sidabid09@Sids-MacBook-Pro-2.local>
2023-12-29 15:41:27 -0600

    initial

---
+++ loopcraft/loopcraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  loopcraftApp.swift
+//  loopcraft
+//
+//  Created by Siddharth Gurumurthi on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct loopcraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
